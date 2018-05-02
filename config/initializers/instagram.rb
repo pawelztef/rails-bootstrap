bcd6022abe9aecb3c1f76abfd68f51ccfcd95050
pawelztef <pawelztef@gmail.com>
2018-05-02 10:44:30 +0100

    keys updated

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,6 @@
 Instagram.configure do |config|
-  config.client_id = 'e4d593c848e04c5a94de5828b7183c2a'
-  config.client_secret = 'f53491490f794f6cb53fac4fdd655cf2' 
+  # config.client_id = 'e4d593c848e04c5a94de5828b7183c2a'
+  config.client_id = '9027da45b40c4a5ead2b06e9cecbd977'
+  # config.client_secret = 'f53491490f794f6cb53fac4fdd655cf2' 
+  config.client_secret = 'b4d31e13a3784b178913372ce01c91ac'
 end
