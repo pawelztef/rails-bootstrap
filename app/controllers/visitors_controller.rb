4be8d4f92c7823980892319890e3008c0f49bd74
pawelztef <pawelztef@gmail.com>
2018-05-02 07:59:32 +0100

    basic auth added

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,14 @@
 class VisitorsController < ApplicationController
+
+  def callback
+    # TODO redirect_uri refactor
+    response = Instagram.get_access_token(params[:code], :redirect_uri => 'http://localhost:3000/callback')
+    session[:access_token] = response.access_token
+    redirect_to feed_url
+  end
+
+  def feed
+byebug
+  end
+
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,4 @@
 Instagram.configure do |config|
-  config.client_id = ENV['CLIENT_ID']
-  config.client_secret = ENV['CLIENT_SECRET']
-  # For secured endpoints only
-  #config.client_ips = '<Comma separated list of IPs>'
+  config.client_id = 'e4d593c848e04c5a94de5828b7183c2a'
+  config.client_secret = 'f53491490f794f6cb53fac4fdd655cf2' 
 end
