e4247f022121ef32c0afa6ab8b401a1c2d631aa9
pawelztef <pawelztef@gmail.com>
2018-05-07 10:26:21 +0100

    cleaning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,6 @@ module ApplicationHelper
   def sign_link
     if session[:access_token].nil?
       content_tag :li do
-        # link_to 'Sign in with Instagram', Instagram.authorize_url(redirect_uri: "http://localhost:3000/callback")
-        # link_to 'Sign in with Instagram', Instagram.authorize_url(redirect_uri: 'https://obscure-peak-36944.herokuapp.com/callback')
         link_to 'Sign in with Instagram', Instagram.authorize_url(redirect_uri: ENV["redirect_uri"])
       end
     else
